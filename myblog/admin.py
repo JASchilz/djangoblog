b43b9d4efc2d41065d487d77ab3eaf5309b7d4be
Joseph Schilz <joseph@schilz.org>
2018-10-21 10:58:11 -0700

    Update admin.py
    Excellent. You got exactly the intended effect. See Start Learning for my notes. (:

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,21 +1,6 @@
 from django.contrib import admin
 from myblog.models import Post, Category
 
-
-#@admin.register(Post, Category)
-#class Post(admin.ModelAdmin):
-#    pass
-##    fieldsets = ((None, {
-##            'fields': ('title', 'text', 'author', 'published_date')
-##            }),
-##            ('Advanced options', {
-##                    'classes': ('collapse',),
-##                    'fields': ('name', 'description', 'posts'),
-##            }),
-##    )
-
-
-
 class PostCatInline(admin.TabularInline):
     model = Category.posts.through
     
@@ -25,23 +10,7 @@ class PostAdmin(admin.ModelAdmin):
     
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
-    inlines =[PostCatInline,]
     exclude = ('posts',)
-    
-
-#admin.site.register(Post)
-#admin.site.register(Category)
-
-#admin.site.register(Post)
-#
-#class PostInline(admin.TabularInline):
-#    model = Category.posts.through
-#    
-#@admin.register(Category)
-#class CategoryAdmin(admin.ModelAdmin):
-#    inlines = (PostInline,)
-#    exclude = ('posts',)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
-#admin.site.register(PairPostCategory)
